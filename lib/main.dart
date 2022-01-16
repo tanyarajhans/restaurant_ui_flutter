e688f0942203a452283707f807ea8969344d9cae
Tanya <tanyarajhans7@gmail.com>
2022-01-16 16:36:03 +0530

    Refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -84,75 +84,89 @@ class MyApp extends StatelessWidget {
               )
             ),
 
-           Container(
-             margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-             padding: EdgeInsets.only(top: 10.0, left: 20.0, bottom: 10.0),
-             decoration: BoxDecoration(
-               borderRadius: BorderRadius.all(Radius.circular(20),
-               
-               ),
-               color: Color(0xff292e3c),
-             ),
-             
-             child: Row(
-               
-               children: <Widget>[
-              
-               ClipRRect(
-                borderRadius: BorderRadius.circular(100.0),
-                
-                child: Image(image: AssetImage('images/me.jpeg'),
-                  height: 100.0,
-                  width: 100.0,
-                ),
-               ),
-               SizedBox(
-                width: 20.0
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: 
-               <Widget>[
-                 Text('Chicken burger', 
-                 textAlign: TextAlign.left,
-                 style: TextStyle(fontSize: 20.0,
-                 color: Colors.white),
-                 ),
-                 Text('320 g',
-                   textAlign: TextAlign.start,
-                 style: TextStyle(fontSize: 15.0,
-                 color: Color(0xffcecfd2)),
-                 
-                 ),
-                 SizedBox(
-                   height: 10.0,
-                 ),
-                 Container(
-                   decoration: BoxDecoration(
-                     color: Color(0xff4d3a86),
-                     borderRadius: BorderRadius.circular(5)
-                   ),
-                   height: 30.0,
-                   width: 80.0,
-                   child: Align(alignment: Alignment.center,
-                   child: Text("\$5.32", 
-                   style: TextStyle(fontSize: 15.0,
-                 color: Colors.white, fontWeight: FontWeight.bold),
-                 )
-                 )
-                 )
-               ],
-               )
-              )
-             ],
-             )
-           ) 
+           ItemCard(),
+           ItemCard(),
+           ItemCard(),
+           ItemCard(),
 
           ],
         )
         )
+    );
+  }
+}
+
+class ItemCard extends StatelessWidget {
+  const ItemCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+      padding: EdgeInsets.only(top: 10.0, left: 20.0, bottom: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20),
+        
+        ),
+        color: Color(0xff292e3c),
+      ),
+      
+      child: Row(
+        
+        children: <Widget>[
+       
+        ClipRRect(
+         borderRadius: BorderRadius.circular(100.0),
+         
+         child: Image(image: AssetImage('images/me.jpeg'),
+           height: 100.0,
+           width: 100.0,
+         ),
+        ),
+        SizedBox(
+         width: 20.0
+       ),
+       Container(
+         alignment: Alignment.centerLeft,
+         child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: 
+        <Widget>[
+          Text('Chicken burger', 
+          textAlign: TextAlign.left,
+          style: TextStyle(fontSize: 20.0,
+          color: Colors.white),
+          ),
+          Text('320 g',
+            textAlign: TextAlign.start,
+          style: TextStyle(fontSize: 15.0,
+          color: Color(0xffcecfd2)),
+          
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xff4d3a86),
+              borderRadius: BorderRadius.circular(5)
+            ),
+            height: 30.0,
+            width: 80.0,
+            child: Align(alignment: Alignment.center,
+            child: Text("\$5.32", 
+            style: TextStyle(fontSize: 15.0,
+          color: Colors.white, fontWeight: FontWeight.bold),
+          )
+          )
+          )
+        ],
+        )
+       )
+      ],
+      )
     );
   }
 }
