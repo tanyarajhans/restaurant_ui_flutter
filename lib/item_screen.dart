fc109e832622105d9e3c104bb296460c507bdfee
Tanya <tanyarajhans7@gmail.com>
2022-01-17 23:01:12 +0530

    Itemscreen UI done

--- lib/item_screen.dart
+++ lib/item_screen.dart
@@ -10,7 +10,7 @@ class ItemScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff1e2025),
       body: Container(
-              margin: EdgeInsets.only(top: 80.0, left: 20.0, right: 20.0),
+              margin: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0),
               child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -34,12 +34,13 @@ class ItemScreen extends StatelessWidget {
 
                       child: 
                     Container(
-                      margin: EdgeInsets.only(top: 60.0, left: 20.0, right: 20.0, bottom: 60.0),
+                      margin: EdgeInsets.only(top: 40.0, left: 0.0, right: 45.0, bottom: 60.0),
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(15.0),
                         
                         child:
-                       Image(image: AssetImage('images/i2.jpeg')
+                       Image(image: AssetImage('images/i1.jpeg'),
+                       height: 300.0,
                        ),
                       ),
                      )
@@ -49,17 +50,41 @@ class ItemScreen extends StatelessWidget {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.center,
                        children: <Widget>[
+                          SizedBox(
+                          height: 30,
+                        child: ElevatedButton(
+                          onPressed: (){}, 
+                         child: Text('New',
+                         style: TextStyle(
+                           fontWeight: FontWeight.bold
+                         )
+                         ),
+                         style: ButtonStyle(
+                           backgroundColor: MaterialStateProperty.all(Color(0xff7a3dff)),
+                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(7.0),
+    )
+  )
+                           
+                         )
+                         )
+                         ),
+                         SizedBox(
+                           height: 20.0
+                         ),
+
                          Text('Big double cheeseburger',
                          textAlign: TextAlign.center,
                          style: TextStyle(color: Colors.white, fontSize: 32.0, fontWeight: FontWeight.bold)),
                          SizedBox(
-                           height: 30.0
+                           height: 20.0
                          ),
                          Text('Marble beef, cheddar cheese, jalapeno pepper, pickled cucumber, lettuce, red onion, BBQ sauce', 
                          textAlign: TextAlign.center,
                          style: TextStyle(color: Color(0xff454953),  fontSize: 18.0, fontWeight: FontWeight.w600)),
                          SizedBox(
-                           height: 30.0
+                           height: 20.0
                          ),
                          Row(
                            mainAxisAlignment: MainAxisAlignment.center,
@@ -94,9 +119,31 @@ class ItemScreen extends StatelessWidget {
                            
                          ],
                          ),
-                         
-                         TextButton(onPressed: (){}, child: Text('Taste it for \$5.42'))
-
+                         SizedBox(
+                           height: 50.0
+                         ),
+                         SizedBox(
+                           
+                           width: 380, // <-- Your width
+                          height: 60,// <-- match-parent
+                        child: ElevatedButton(
+                          onPressed: (){}, 
+                         child: Text('Taste it for \$5.42',
+                         style: TextStyle(
+                           fontSize: 20.0,
+                           fontWeight: FontWeight.bold
+                         )),
+                         style: ButtonStyle(
+                           backgroundColor: MaterialStateProperty.all(Color(0xff7a3dff)),
+                           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+    RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(18.0),
+    )
+  )
+                           
+                         )
+                         )
+                         )
                       ],
                       ),
                     
